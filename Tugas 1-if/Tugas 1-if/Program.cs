a15ee355d74dfba7b05a8f824750dfe081257be6
nafakhatul <nafakhatul258723@student.smkn1kandeman.sch.id>
2025-11-18 07:41:01 +0700

    Add files via upload

---
+++ Tugas 1-if/Tugas 1-if/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_1_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan angka: ");
+            int angka = int.Parse(Console.ReadLine());
+
+            if (angka >=  100) 
+            {
+                Console.WriteLine("Angka lebih besar dari 100");
+            }
+            else
+            {
+                Console.WriteLine("Angka lebih kecil dari 100");
+            }
+        }
+    }
+}
